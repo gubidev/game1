0f856b2a5ca6cd2190ce06994b3bec4c2e4fcdf6
Felipe Caiafa <felipe.soares@sou.inteli.edu.br>
2025-03-07 12:59:13 -0300

    ctgfyjhg

--- cenas/plataforma.js
+++ cenas/plataforma.js
@@ -20,8 +20,8 @@ class plataforma extends Phaser.Scene {
     preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', './assets/star.png');
-        this.load.image('bomb', '/assets/bomb.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
