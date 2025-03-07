0724056d9846b8e7e2bb8d51e4a2b674f259285e
Felipe Caiafa <felipe.soares@sou.inteli.edu.br>
2025-03-07 12:11:56 -0300

    ..

--- game.js
+++ game.js
@@ -12,11 +12,11 @@ class game extends Phaser.Scene {
 
     preload() {
         // Carrega as imagens e sprites necessárias para o jogo
-        this.load.image('sky', '/assets/sky.png');
-        this.load.image('ground', '/assets/platform.png');
-        this.load.image('bomb', '/assets/bomb.png');
-        this.load.image('princess', '/assets/princess.png');
-        this.load.spritesheet('dude', '/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('sky', '../assets/sky.png');
+        this.load.image('ground', '../assets/platform.png');
+        this.load.image('bomb', '../assets/bomb.png');
+        this.load.image('princess', '../assets/princess.png');
+        this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
     create() {
